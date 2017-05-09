d306ef1b0cd73c8d5040d8c7626f38bab36ab45b
yates <yates.lyc@gmail.com>
2017-05-09 15:11:07 +0800

    GameHelp Scene initialization

---
+++ Classes/GameHelp.cpp
@@ -0,0 +1,29 @@
+//
+//  GameHelp.cpp
+//  SuperMarioFamily
+//
+//  Created by 梁永潮 on 2017/5/9.
+//
+//
+
+/*
+ 
+ 
+ 
+ */
+
+#include "GameHelp.hpp"
+
+bool GameHelp::init()
+{
+    if(!cocos2d::Layer::create())
+    {
+        return false;
+        
+    }
+    
+    auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
+    
+    au
+}
+
---
+++ Classes/GameHelp.hpp
@@ -0,0 +1,26 @@
+//
+//  GameHelp.hpp
+//  SuperMarioFamily
+//
+//  Created by 梁永潮 on 2017/5/9.
+//
+//
+
+#ifndef GameHelp_hpp
+#define GameHelp_hpp
+#include "HelloWorldScene.h"
+#include <stdio.h>
+class GameHelp: cocos2d::Layer
+{
+public:
+    static cocos2d::Scene * creatScene();
+    
+    virtual bool init();
+    
+    void menuCallBack(cocos2d::Ref * pSender);
+    
+    CREATE_FUNC(GameHelp);
+    
+};
+
+#endif /* GameHelp_hpp */
