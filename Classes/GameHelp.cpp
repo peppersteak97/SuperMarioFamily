05620c6a2aeedb31498a25b84d4222bd448c1e65
yates <yates.lyc@gmail.com>
2017-05-10 13:38:12 +0800

    fix some problems of project file can't be opened

--- Classes/GameHelp.cpp
+++ Classes/GameHelp.cpp
@@ -24,7 +24,21 @@ bool GameHelp::init()
     
     auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
     
-    auto backLable = 
+    auto lableBack = cocos2d::Label::createWithSystemFont("Back To Menu","Arial",15);
+    
+    auto itemBack = cocos2d::MenuItemLabel::create(lableBack, CC_CALLBACK_1(GameHelp::menuCallBack, this));
+    
+    auto menu = cocos2d::Menu::create(itemBack,nullptr);
+    
+    menu->setAnchorPoint(cocos2d::Point::ANCHOR_BOTTOM_LEFT);
+    
+    menu->setPosition(cocos2d::Vec2(visibleSize.width,0));
+    
+    this->addChild(menu);
+    
+    return true;
     
 }
+
+
 
