f9640c02631726e89906acb390af20d2da5bc5e7
LeonWongInspiration <LeonWong@Leon-Wongs-MacBook-Pro.local>
2017-05-25 19:06:50 +0800

    Create Hero Superclass
    Add a superclass for hero

---
+++ Classes/Hero.h
@@ -0,0 +1,27 @@
+/**
+ * @brief Hero superclass
+ * @file Classes/Hero.cpp
+ * @Author Leon Wong
+ *
+ * <ChangeLog>
+ * -----------------------------------------------------------------------------------------------
+ * |   <Build>   |   <Author>   | <Description>                                                  |
+ * -----------------------------------------------------------------------------------------------
+ * |201705251846 |   Leon Wong  | FIrst time initialization                                      |
+ * |             |              |                                                                |
+ * |             |              |                                                                |
+ * -----------------------------------------------------------------------------------------------
+ */
+
+#pragma once
+
+#ifndef Hero_h
+#define Hero_h
+
+USING_NS_CC;
+
+class Hero: public Layer{
+    
+};
+
+#endif /* Hero_h */
