a01aec2248a8987f9059ea396359717900b58a6a
LeonWongInspiration <LeonWong@Leon-Wongs-MacBook-Pro.local>
2017-05-25 19:29:50 +0800

    Hero Properties
    Add some properties for hero

--- Classes/Hero.h
+++ Classes/Hero.h
@@ -7,9 +7,8 @@
  * -----------------------------------------------------------------------------------------------
  * |   <Build>   |   <Author>   | <Description>                                                  |
  * -----------------------------------------------------------------------------------------------
- * |201705251846 |   Leon Wong  | FIrst time initialization                                      |
- * |             |              |                                                                |
- * |             |              |                                                                |
+ * |201705251928 |   Leon Wong  | Add some properties for hero                                   |
+ * |201705251846 |   Leon Wong  | First time initialization                                      |
  * -----------------------------------------------------------------------------------------------
  */
 
@@ -18,9 +17,44 @@
 #ifndef Hero_h
 #define Hero_h
 
+#include "Enemy.h"
+#include "Goomba.h"
+#include "Koopa.h"
+#include "Koopa_dead.h"
+#include "Piranha.h"
+#include "cocos2d.h"
+#include "SimpleAudioEngine.h"
+
+#include <list>
+
 USING_NS_CC;
+using namespace std
+
+/**
+ * @brief An enum denoting the hero's status.
+ *
+ */
+enum HeroState{
+    STAND,
+    JUMP,
+    MOVE,
+    DIE,
+    FALL,
+};
 
 class Hero: public Layer{
+public:
+    /// Lives Count
+    static int life = 2;
+    /// Sprite for hero
+    Sprite* heroSprite;
+    /// Hero State
+    HeroState heroState;
+    
+    /// true if press down
+    bool down;
+    /// true if passive
+    bool passive;
     
 };
 
