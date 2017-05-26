68168a4a54fbbd7fe11dd2155e4fb083e0f65fe4
LeonWongInspiration <LeonWong@Leon-Wongs-MacBook-Pro.local>
2017-05-26 13:25:15 +0800

    Create Utility
    There is a Utility called Direction (enum).

---
+++ Classes/Utility.h
@@ -0,0 +1,29 @@
+/**
+ * @brief Some Utilities
+ * @file Utility.h
+ * @author Leon Wong
+ *
+ * <changelog>
+ *---------------------------------------------------------------------------------------------------------
+ *     <Build>    |  <Author>  |  <Description>                                                           |
+ *--------------------------------------------------------------------------------------------------------|
+ *  201705261319  |  Leon Wong | Initialization                                                           |
+ *---------------------------------------------------------------------------------------------------------
+ */
+ 
+
+#ifndef Utility_h
+#define Utility_h
+
+/**
+ * @brief An enum denoting which direction the object is going
+ * 
+ */
+enum Direction{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+#endif /* Utility_h */
