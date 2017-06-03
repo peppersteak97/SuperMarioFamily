d50563e1202bd9c413f5a4d5cd0156a555f87260
Feng <1041090456@qq.com>
2017-06-03 17:47:17 +0800

    Map
    Level1

---
+++ Resources/Map/Blocks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Blocks" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="C:/Users/10410/Documents/GitHub/SuperMarioFamily/Resources/Tilesets/Blocks.png" width="192" height="192"/>
+</tileset>
